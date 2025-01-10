e2e9220950bc46b18d8515522004e3a629b67f75
Vera <althinivera77@gmail.com>
2025-01-10 12:09:57 +0100

    Added img-tags to display images on the ProductDetails-page, changed the Navbar layout and started working on the ContactForm, RegisterForm and LoginForm components

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,22 @@
 function LoginForm() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div>LoginForm</div>
+    <>
+        <form>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="text" id="password" />
+            </div>
+            <button>Login</button>
+        </form>
+    </>
   )
 }
 export default LoginForm
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,6 +1,37 @@
 function RegisterForm() {
+
+  const [firstName, setFirstName] = useState('')
+  const [surname, setSurname] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
   return (
-    <div>RegisterForm</div>
+    <>
+        <form>
+            <div>
+                <label htmlFor="firstName">First name</label>
+                <input type="text" id="firstName" />
+            </div>
+            <div>
+                <label htmlFor="surname">Surname</label>
+                <input type="text" id="surname" />
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="text" id="password" />
+            </div>
+            <div>
+                <label htmlFor="confirmPassword">Confirm password</label>
+                <input type="text" id="confirmPassword" />
+            </div>
+            <button>Create account</button>
+        </form>
+    </>
   )
 }
 export default RegisterForm
