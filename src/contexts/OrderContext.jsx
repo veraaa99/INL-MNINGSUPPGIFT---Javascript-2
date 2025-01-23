13e5689f30bf9d51b41706af4fa9a7b02575fecb
Vera <althinivera77@gmail.com>
2025-01-23 15:44:33 +0100

    FIxed the logic to send several products in one order (when sending a POST-request to the API)

--- src/contexts/OrderContext.jsx
+++ src/contexts/OrderContext.jsx
@@ -1,114 +1,65 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import Products from "../pages/Products";
 
 export const OrderContext = createContext()
 
 function OrderContextProvider({ children }) {
 
     const [order, setOrder] = useState({})
-    const [orders, setOrders] = useState([])
+    const [orders, setOrders] = useState([{}])
     const [shoppingCart, setShoppingCart] = useState([])
 
     let [totalQuantity, setTotalQuantity] = useState(0)
     let [productQuantity, setProductQuantity] = useState(0)
 
-    const saveOrder = async (values, id) => { 
+    const placeOrder = (values) => { 
 
-        const newOrder = {
-            products: [
-                { 
-                productId: id, 
-                quantity: 1 
-                }
-            ]
-        }
+        console.log(values)
+
+        const newData = values.map((item)=>{
+            return {
+                productId: item.product.product._id,
+                quantity: item.quantity
+            }
+        })
+          
+        setOrders(newData)
+
+        // https://stackoverflow.com/questions/76192706/how-do-i-create-a-different-array-from-an-existing-array-in-react
+        const newOrder = values.map((item) => {
+            return {productId: item.product.product._id, quantity: item.quantity}
+          })
 
         console.log(newOrder)
-        console.log(values)
-
-        const res = await fetch('https://js2-ecommerce-api.vercel.app/api/orders', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(newOrder)
-        }).catch((err) => console.log('error'))
-
-        const data = await res.json()
-        console.log(data)
-
-            //  addOrder(data, values)
-
-            //  const item = shoppingCart.find((product) => product.products.productId === newOrder.products.productId)
-            //  console.log(item)
-
-            //  if(item) {
-            //    item.products.quantity++
-            //    console.log('bla')
-            //  } else {
-            //     setShoppingCart([newOrder])
-            //     console.log('bla2')
-            //  }
-            // //  addOrder(data)
-
-            // //  addOrder(newOrder)
-            //   setOrder(data)
-            //   setOrders([...orders, data])
-            // // console.log(newOrder)
-            // // console.log(orders)
-               
+        sendOrder(newOrder)
     }
 
-    // const placeOrder = (product) => {
-    //     setOrders(state => {
-    //       return[...state, { product }]
-    //     })
-    // }
+    const sendOrder = async (newOrder) => {
 
-    const addOrder = (data, values) => {
-        console.log(data)
-        console.log(values)
-        // const bla = newOrder.products.map(x => x.quantity);
-
-        // console.log(newOrder.products[0].quantity)
-
-        const item = shoppingCart.find((product) => product === values)
-        console.log(item)
-
-        if(item) {
-        // setQuantity(quantity++)
-        // console.log(quantity)
-        } else {
-        setShoppingCart([...shoppingCart, values])         
-        console.log(shoppingCart)
+        const order = {
+            products: newOrder
         }
 
-        setOrder(data)
-        setOrders([...orders, data])
+        console.log(order)
+
+        const res = await fetch('https://js2-ecommerce-api.vercel.app/api/orders', {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(order)
+            }).catch((err) => console.log('error'))
+
+        const data = await res.json()
+        console.log(data) 
     }
-
-    useEffect(() => {
-        console.log(orders);
-    }, [orders]);
-
-    useEffect(() => {
-        console.log(order);
-    }, [order]);
-
-    useEffect(() => {
-        console.log(shoppingCart);
-    }, [shoppingCart]);
-
-    // useEffect(() => {
-    //     console.log(quantity);
-    // }, [quantity]);
 
     const value = {
         orders,
         setOrders,
         order,
         setOrder,
-        saveOrder,
-        addOrder,
+        placeOrder,
     }
 
     return (
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,13 +3,44 @@ import { useOrderContext } from "../contexts/OrderContext"
 import ShoppingCartList from "../components/ShoppingCartList";
 import { ShoppingCartContext, useShoppingCartContext } from "../contexts/ShoppingCartContext";
 import SetQuantityButton from "../components/SetQuantityButton";
+import { useMessageContext } from "../contexts/MessageContext";
+import useForm from "../hooks/useForm";
+import FormInput from "../components/FormInput";
 
 function Checkout() {
 
+  const { placeOrder } = useOrderContext()
+  const { shoppingItems } = useShoppingCartContext()
+  const { sendMessage } = useMessageContext()
+  
+  const { form, errors, handleChange, handleSubmit } = useForm({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    placeOrder(shoppingItems)
+
+    handleSubmit(e, async (values) => {
+      sendMessage(values)
+    })
+  }
+
   return (
-    <div>
-       <ShoppingCartList />
-    </div>
+    <>
+      <div>
+        <form onSubmit={onSubmit}>
+        <ShoppingCartList />
+
+        <FormInput label="Name" name="name" id="name" type="text" value={form.name} onChange={handleChange} errorMsg={errors.name}/>
+        <FormInput label="Email" name="email" id="email" type="email" value={form.email} onChange={handleChange} errorMsg={errors.email} />
+        <FormInput label="Message" name="message" id="message" type="text" value={form.message} onChange={handleChange} errorMsg={errors.message}/>
+          <button type="submit">Place order</button>
+        </form>
+      </div>
+    </>
   )
 }
 export default Checkout
